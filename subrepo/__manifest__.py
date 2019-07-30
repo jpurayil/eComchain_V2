48b02631548ea9a6a9451e33c1754bcab2424602
Jithin Purayil <jpurayil@ibizsoftinc.com>
2019-07-30 11:28:32 +0530

    Manifest created

---
+++ subrepo/__manifest__.py
@@ -0,0 +1,50 @@
+# Copyright 2019 eCocmhain
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+{
+    "name": """eComchain Odoo Connector""",
+    "summary": """eComchain plugin for Odoo""",
+    "category": "Test",
+    "description": """eComchain plugin for managing e-commerce transactions""",
+    "images": [],
+    "version": "8.0",
+    "application": False,
+
+    "author": "eComchain",
+    "support": "support@ecomchain.com",
+    "website": "https://ecomchain.com/",
+    "license": "LGPL-3",
+    #"depends": [
+    #    "{DEPENDENCY1}",
+    #    "{DEPENDENCY2}",
+    #],
+    "external_dependencies": {"python": [], "bin": []},
+    #"data": [
+    #    "{FILE1}.xml",
+    #    "{FILE2}.xml",
+    #],
+    #"demo": [
+    #    "demo/{DEMOFILE1}.xml",
+    #],
+    #"qweb": [
+    #    "static/src/xml/{QWEBFILE1}.xml",
+    #],
+
+    "post_load": None,
+    "pre_init_hook": None,
+    "post_init_hook": None,
+    "uninstall_hook": None,
+
+    "auto_install": False,
+    "installable": True,
+
+    # "demo_title": "{MODULE_NAME}",
+    # "demo_addons": [
+    # ],
+    # "demo_addons_hidden": [
+    # ],
+    # "demo_url": "DEMO-URL",
+    # "demo_summary": "{SHORT_DESCRIPTION_OF_THE_MODULE}",
+    # "demo_images": [
+    #    "images/MAIN_IMAGE",
+    # ]
+}
